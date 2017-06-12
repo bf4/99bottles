6056199fc30433afc647764ddac77cad6ce7be24
Benjamin Fleischer <github@benjaminfleischer.com>
2017-06-11 21:24:54 -0500

    Test two

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,9 @@
 class Bottles
   def verse(verse_number)
+    number_of_bottles_on_wall = verse_number
     <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{number_of_bottles_on_wall} bottles of beer on the wall, #{number_of_bottles_on_wall} bottles of beer.
+Take one down and pass it around, #{number_of_bottles_on_wall - 1} bottles of beer on the wall.
     VERSE
   end
 end
