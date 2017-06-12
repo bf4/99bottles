edaee88d3c6fb2238ea98596ef3fc824619b9cb8
Benjamin Fleischer <github@benjaminfleischer.com>
2017-06-11 21:43:28 -0500

    Test four: starting to look ugly

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,8 +3,8 @@ class Bottles
     number_of_bottles_on_wall = verse_number
     bottles_left_on_wall = number_of_bottles_on_wall - 1
     <<-VERSE
-#{number_of_bottles_on_wall} bottles of beer on the wall, #{number_of_bottles_on_wall} bottles of beer.
-Take one down and pass it around, #{bottles_left_on_wall} bottle#{bottles_left_on_wall == 1 ? '' : 's'} of beer on the wall.
+#{number_of_bottles_on_wall} bottle#{number_of_bottles_on_wall == 1 ? '' : 's'} of beer on the wall, #{number_of_bottles_on_wall} bottle#{number_of_bottles_on_wall == 1 ? '' : 's'} of beer.
+Take #{bottles_left_on_wall.zero? ? 'it' : 'one'} down and pass it around, #{bottles_left_on_wall.zero? ? 'no more' : bottles_left_on_wall} bottle#{bottles_left_on_wall == 1 ? '' : 's'} of beer on the wall.
     VERSE
   end
 end
