7840c6882facf01521e34df8f812cb0181932a5a
Benjamin Fleischer <github@benjaminfleischer.com>
2017-06-11 21:47:34 -0500

    Test five: last verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,25 @@
 class Bottles
   def verse(verse_number)
+    if verse_number.zero?
+      shopping_verse
+    else
+      drinking_verse(verse_number)
+    end
+  end
+
+  def drinking_verse(verse_number)
     number_of_bottles_on_wall = verse_number
     bottles_left_on_wall = number_of_bottles_on_wall - 1
     <<-VERSE
 #{number_of_bottles_on_wall} bottle#{number_of_bottles_on_wall == 1 ? '' : 's'} of beer on the wall, #{number_of_bottles_on_wall} bottle#{number_of_bottles_on_wall == 1 ? '' : 's'} of beer.
 Take #{bottles_left_on_wall.zero? ? 'it' : 'one'} down and pass it around, #{bottles_left_on_wall.zero? ? 'no more' : bottles_left_on_wall} bottle#{bottles_left_on_wall == 1 ? '' : 's'} of beer on the wall.
+    VERSE
+  end
+
+  def shopping_verse
+    <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
   end
 end
